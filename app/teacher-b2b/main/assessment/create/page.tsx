8242a6c81029992f8973c3f0e0da0e85df025375
gajendra <gajendra@inest.com>
2025-06-06 16:28:00 +0530

    10 pages created
    assessment-saved
    student assessment progress report
    create assessment 4
    create test 5
    create test 6
    create ai generated assessment
    folder name
    student quiz progress report
    create ai generated quiz
    upload from existing quiz

--- app/teacher-b2b/main/assessment/create/page.tsx
+++ app/teacher-b2b/main/assessment/create/page.tsx
@@ -1,4 +1,4 @@
-import CreateAssessmentPage from "@/components/teacher-b2b/assessment/create-assessment-4";
+import CreateAssessmentPage from "@/components/teacher-b2b/main/create-assessment";
 
 export default function CreateAssessment() {
     return <div>
